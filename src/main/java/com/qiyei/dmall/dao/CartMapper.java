833ef9961294ff0c4f814df7775434319309b8ba
qiyei2015 <1273482124@qq.com>
2020-02-12 21:50:18 +0800

    初始化工程

---
+++ src/main/java/com/qiyei/dmall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.qiyei.dmall.dao;
+
+import com.qiyei.dmall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
